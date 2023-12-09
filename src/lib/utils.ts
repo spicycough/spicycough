306cd969c796263113c93636915a69321d59af99
timredd <timredd13@gmail.com>
2023-12-09 11:54:07 -0500

    Delete unneeded lib/ files

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Extracts the domain from a given URL, removing the 'www' prefix if present.
- * It works with different URL formats, including those with http, https,
- * and with or without the 'www' prefix.
- */
-export const getDomainFromUrl = (url: string): string => {
-	const hostname = new URL(url).hostname;
-	return hostname.replace(/^www\./, "");
-};
