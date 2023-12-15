1d75b7a79c99914e3d5b8afff2e3eb8b4506edcb
timredd <timredd13@gmail.com>
2023-12-14 22:27:32 -0500

    Add working scraper

--- packages/spicycrawler/src/main.ts
+++ packages/spicycrawler/src/main.ts
@@ -1,15 +1,55 @@
-// For more information, see https://crawlee.dev/
-import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
+import * as cheerio from "cheerio";
+import { gotScraping } from "got-scraping";
+import { P, match } from "ts-pattern";
 
-import { router } from './routes.js';
+const hostnames = {
+	NATURE: "nature.com",
+} as const;
 
-const startUrls = ['https://crawlee.dev'];
+type RouteContext = {
+	url: URL;
+	response: Response;
+};
 
-const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
-});
+type ParsingFunction = (context: RouteContext) => Promise<unknown>;
 
-await crawler.run(startUrls);
+const nature = async ({ response }: RouteContext) => {
+	const body = await response.text();
+	const $ = cheerio.load(body);
+
+	const title = $("h1.c-article-title").text().trim();
+	const author = $("li.c-article-author-list__item")
+		.map((_, el) => $(el).text().trim())
+		.get(0);
+
+	const articleData = {
+		url: response.url,
+		title,
+		author,
+		// publicationDate,
+		// sourceUrl,
+		// abstract,
+		// fullText,
+	};
+
+	return articleData;
+};
+
+const route = async (url: string) => {
+	console.log(`Processing ${url}`);
+	const parsedUrl = new URL(url);
+
+	const response = await gotScraping(parsedUrl.href);
+
+	const includes = P.string.includes;
+	const parsingFn: ParsingFunction = match(parsedUrl.hostname)
+		.with(includes(hostnames.NATURE), () => nature)
+		.otherwise(() => async () => {
+			console.error(`No parsing function for ${parsedUrl.hostname}`);
+		});
+
+	return parsingFn({ url: parsedUrl, response });
+};
+
+const r = await route("https://www.nature.com/articles/d41586-021-01513-4");
+console.log(r);
