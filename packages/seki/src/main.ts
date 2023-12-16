98457e38d61748f7dc0a3ee5ee435a45859b448f
timredd <timredd13@gmail.com>
2023-12-16 14:24:01 -0500

    Rename spicycrawler to @seki

--- packages/seki/src/main.ts
+++ packages/seki/src/main.ts
@@ -1,4 +1,4 @@
-import { gotScraping } from "got-scraping";
+import { gotScraping, type ExtendedOptionsOfTextResponseBody } from "got-scraping";
 import { P, match } from "ts-pattern";
 import { router } from "./router";
 
@@ -8,11 +8,13 @@ const hostnames = {
 	NATURE: "nature.com",
 } as const;
 
-export const scrape = async (url: string) => {
+export type ScrapeOptions = ExtendedOptionsOfTextResponseBody;
+
+export const scrape = async (url: string, options: ScrapeOptions) => {
 	console.info(`Processing ${url}`);
 	const parsedUrl = new URL(url);
 
-	const response = await gotScraping(parsedUrl.href);
+	const response = await gotScraping(parsedUrl.href, options);
 
 	const parsingFn = match(parsedUrl.hostname)
 		.with(includes(hostnames.NATURE), () => router.nature)
