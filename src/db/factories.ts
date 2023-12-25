878527ba1be95e33b3d98d77540c9e4a7afed404
timredd <timredd13@gmail.com>
2023-12-24 23:22:07 -0500

    Rename content item type to kind

--- src/db/factories.ts
+++ src/db/factories.ts
@@ -7,13 +7,13 @@ import {
 	rand,
 	randNumber,
 } from "@ngneat/falso";
-import { ContentItemKind, type ContentItem, type ContentItemId } from "./schema";
+import { ContentItemKind, type ContentItem, type ContentItemId } from "./schema/contentItems";
 
 export const createFakeContentItem = (): ContentItem => {
 	const id = randNumber() as ContentItemId;
-	const type = rand(Object.values(ContentItemKind));
+	const kind = rand(Object.values(ContentItemKind));
 	const title = randTextRange({ min: 3, max: 15 });
-	const author = randFullName();
+	const authors = randFullName();
 	const slug = title.toLowerCase().replace(/\s/g, "-");
 	const sourceUrl = "http://unsplash.it/200/200";
 	const abstract = randLines();
@@ -24,9 +24,9 @@ export const createFakeContentItem = (): ContentItem => {
 
 	return {
 		id,
-		type,
+		kind,
 		title,
-		author,
+		authors,
 		sourceUrl,
 		abstract,
 		fullText,
