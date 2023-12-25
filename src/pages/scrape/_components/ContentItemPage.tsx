f349875d842cb3a157ebcd12b706e1895b8b8734
timredd <timredd13@gmail.com>
2023-12-25 01:14:41 -0500

    Adjust styling of page and view content item components

--- src/pages/scrape/_components/ContentItemPage.tsx
+++ src/pages/scrape/_components/ContentItemPage.tsx
@@ -4,39 +4,46 @@ import { Queue } from "./ContentItemQueue";
 import { View } from "./ContentItemView";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { ListStartIcon } from "lucide-react";
+import { BulkAddDialog } from "./BulkAddDialog";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 
 export const ContentItemPage = ({ className }: ComponentPropsWithoutRef<"div">) => {
 	return (
-		<div
-			className={cn(
-				"p flex h-full flex-col rounded-lg bg-gradient-to-b from-twilight-800 to-twilight-900 p-4",
-				className,
-			)}
-		>
-			<View className="flex-1" />
+		<ResizablePanelGroup direction="vertical">
+			<div
+				className={cn(
+					"flex h-full flex-col rounded-lg bg-gradient-to-b from-twilight-800 to-twilight-900 p-4",
+					className,
+				)}
+			>
+				<ResizablePanel className="">
+					<View className="" />
+				</ResizablePanel>
+				<ResizableHandle withHandle={true} />
+				<ResizablePanel className="">
+					<div className="flex flex-col justify-end">
+						<div className="border-x border-b">
+							<Queue className="h-full" />
+						</div>
 
-			<div className="min-h-1/5 flex-0 h-1/3">
-				<Queue className="h-full border" />
+						<div className="flex w-full justify-end space-x-4 border-x border-b pr-4">
+							<Separator orientation="vertical" />
+							<div className="p-2">
+								<BulkAddDialog>
+									<Button
+										variant="outline"
+										size="default"
+										className="rounded-lg dark:bg-warmth-500 dark:text-midnight-800"
+									>
+										<ListStartIcon className="size-4" />
+									</Button>
+								</BulkAddDialog>
+							</div>
+						</div>
+					</div>
+				</ResizablePanel>
 			</div>
-
-			<div className="flex w-full justify-end space-x-4 border-x pr-4">
-				<Separator orientation="vertical" />
-				<div className="p-2">
-					<Button variant="outline" size="default">
-						<PlusIcon className="size-4" />
-					</Button>
-				</div>
-				<Separator orientation="vertical" />
-				<div className="p-2">
-					<Button variant="outline" size="default">
-						<PlusIcon className="size-4" />
-					</Button>
-				</div>
-			</div>
-			<div className="">
-				<Separator />
-			</div>
-		</div>
+		</ResizablePanelGroup>
 	);
 };
--- src/pages/scrape/_components/ContentItemView.tsx
+++ src/pages/scrape/_components/ContentItemView.tsx
@@ -4,13 +4,12 @@ import { useQueue } from "../_hooks/useQueue";
 import { cn } from "@/lib/utils";
 
 export const View = ({ className }: ComponentPropsWithoutRef<"div">) => {
-	const { selection } = useQueue();
+	const { rowSelection } = useQueue();
 
 	return (
 		<div className={cn("grid h-full grid-cols-[1fr,min-content,1fr] gap-3 pt-6", className)}>
 			<div className="overflow-y-scroll rounded-lg p-1 pl-3">
 				<h2 className="font-bold text-white opacity-100">Abstract</h2>
-				<p>{selection?.abstract ?? "empty"}</p>
 			</div>
 			<Separator orientation="vertical" className="h-full self-stretch" />
 			<div className="overflow-y-scroll rounded-lg p-1 pl-3">
