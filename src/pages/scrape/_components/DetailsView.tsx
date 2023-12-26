b28257c554712bd0a272da1c196edbbcc8ced7f2
timredd <timredd13@gmail.com>
2023-12-26 09:56:26 -0500

    Refactor panels into separate component

--- src/pages/scrape/_components/DetailsView.tsx
+++ src/pages/scrape/_components/DetailsView.tsx
@@ -1,25 +1,23 @@
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { cn } from "@/lib/utils";
-import { MagicWandIcon, TextAlignBottomIcon } from "@radix-ui/react-icons";
-import type { ComponentPropsWithoutRef } from "react";
+import { MagicWandIcon, SymbolIcon, TextAlignBottomIcon } from "@radix-ui/react-icons";
+import type { ComponentPropsWithoutRef, PropsWithChildren, ReactNode } from "react";
 import { useQueue } from "../_hooks/useQueue";
+import { Button } from "@/components/ui/button";
 
 type PanelProps = PropsWithChildren<{
 	title: string;
-	icon: React.ReactNode;
+	icon?: ReactNode;
 }>;
 
 const Panel = ({ title, icon, children }: PanelProps) => {
 	return (
-		<ResizablePanel className="flex flex-col rounded-lg pl-3">
-			<div className="flex space-x-2 align-middle">
-				<TextAlignBottomIcon />
-				<h2 className="font-bold text-white">{title}</h2>
+		<ResizablePanel minSize={3} className="flex flex-col space-y-3 rounded-lg px-6 pb-3 pt-6">
+			<div className="flex items-center space-x-3 align-text-top">
+				{icon}
+				<h2 className="font-bold uppercase text-white">{title}</h2>
 			</div>
-			<div className="overflow-y-scroll py-6 pr-6">
-				{children}
-				{/*<p>{selected?.fullText}</p>*/}
-			</div>
+			<div className="overflow-y-scroll pr-6">{children}</div>
 		</ResizablePanel>
 	);
 };
@@ -28,39 +26,24 @@ export const DetailsView = ({ className }: ComponentPropsWithoutRef<"div">) => {
 	const { selected } = useQueue();
 
 	return (
-		<div className={cn("max-h-full gap-3 overflow-y-scroll pt-6", className)}>
-			<ResizablePanelGroup direction="horizontal" className="pb-3 pt-6">
-				<ResizablePanel className="flex flex-col rounded-lg pl-3">
-					<div className="flex space-x-2 align-middle">
-						<TextAlignBottomIcon />
-						<h2 className="font-bold text-white">All</h2>
-					</div>
-					<div className="overflow-y-scroll py-6 pr-6">
-						<p>{selected?.fullText}</p>
-					</div>
-				</ResizablePanel>
+		<div className={cn("relative max-h-full overflow-y-hidden border-x border-t pt-3", className)}>
+			<Button variant="ghost" size="icon" className="absolute bottom-2 right-2">
+				<SymbolIcon />
+			</Button>
+			<ResizablePanelGroup direction="horizontal">
+				<Panel title="All" icon={<TextAlignBottomIcon />}>
+					<p>{selected?.fullText}</p>
+				</Panel>
 				<ResizableHandle withHandle={true} />
-				<ResizablePanel className="flex flex-col rounded-lg pl-3">
-					<ResizablePanelGroup direction="vertical" className="pb-3">
-						<ResizablePanel className="flex flex-col overflow-y-scroll rounded-lg pl-3 pt-6">
-							<div className="flex space-x-2 align-middle">
-								<MagicWandIcon />
-								<h2 className="font-bold text-white">Summary</h2>
-							</div>
-							<div className="overflow-y-scroll py-6 pr-6">
-								<p className="">{selected?.abstract}</p>
-							</div>
-						</ResizablePanel>
+				<ResizablePanel className="flex flex-col rounded-lg">
+					<ResizablePanelGroup direction="vertical">
+						<Panel title="Abstract" icon={<MagicWandIcon />}>
+							<p className="">{selected?.abstract}</p>
+						</Panel>
 						<ResizableHandle withHandle={true} />
-						<ResizablePanel className="flex flex-col overflow-y-scroll rounded-lg pl-3">
-							<div className="flex space-x-2 pt-6 align-middle">
-								<MagicWandIcon />
-								<h2 className="font-bold text-white">Summary</h2>
-							</div>
-							<div className="overflow-y-scroll py-6 pr-6">
-								<p className="">{selected?.abstract}</p>
-							</div>
-						</ResizablePanel>
+						<Panel title="Summary" icon={<MagicWandIcon />}>
+							<p className="">{selected?.abstract}</p>
+						</Panel>
 					</ResizablePanelGroup>
 				</ResizablePanel>
 			</ResizablePanelGroup>
