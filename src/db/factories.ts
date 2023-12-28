48165a83c83091b75742ecd7926ee3e7a26dd60b
timredd <timredd13@gmail.com>
2023-12-28 11:01:14 -0500

    Refactor content item factory to bulk create

--- src/db/factories.ts
+++ src/db/factories.ts
@@ -2,37 +2,39 @@ import {
 	randFullName,
 	randRecentDate,
 	randParagraph,
-	randLines,
+	randLine,
 	randTextRange,
 	rand,
 	randNumber,
 } from "@ngneat/falso";
 import { ContentItemKind, type ContentItem, type ContentItemId } from "./schema/contentItems";
 
-export const createFakeContentItem = (): ContentItem => {
-	const id = randNumber() as ContentItemId;
-	const kind = rand(Object.values(ContentItemKind));
-	const title = randTextRange({ min: 3, max: 15 });
-	const authors = randFullName();
-	const slug = title.toLowerCase().replace(/\s/g, "-");
-	const sourceUrl = "http://unsplash.it/200/200";
-	const abstract = randLines();
-	const fullText = randParagraph();
-	const publishedAt = randRecentDate().toISOString();
-	const createdAt = Date.now().toString();
-	const updatedAt = Date.now().toString();
+export const createFakeContentItems = (amount?: number): ContentItem[] => {
+	return Array.from({ length: amount ?? 1 }, () => {
+		const id = randNumber() as ContentItemId;
+		const kind = rand(Object.values(ContentItemKind));
+		const title = randTextRange({ min: 3, max: 15 });
+		const authors = randFullName();
+		const slug = title.toLowerCase().replace(/\s/g, "-");
+		const sourceUrl = "http://unsplash.it/200/200";
+		const abstract = randLine();
+		const fullText = randParagraph();
+		const publishedAt = randRecentDate().toISOString();
+		const createdAt = Date.now().toString();
+		const updatedAt = Date.now().toString();
 
-	return {
-		id,
-		kind,
-		title,
-		authors,
-		sourceUrl,
-		abstract,
-		fullText,
-		slug,
-		publishedAt,
-		createdAt,
-		updatedAt,
-	};
+		return {
+			id,
+			kind,
+			title,
+			authors,
+			sourceUrl,
+			abstract,
+			fullText,
+			slug,
+			publishedAt,
+			createdAt,
+			updatedAt,
+		};
+	});
 };
