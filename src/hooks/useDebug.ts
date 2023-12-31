13f6245cd664ce3bc21078272d6d205d6922221c
timredd <timredd13@gmail.com>
2023-12-31 10:05:19 -0500

    Add useDebug hook

---
+++ src/hooks/useDebug.ts
@@ -0,0 +1,18 @@
+import { writeFile } from "fs";
+
+export const useDebug = () => {
+	// Creates filename based on current timestamp
+	const fn = new Date().toISOString().replace(/:/g, "-") + ".md";
+
+	const write = (data: string, filename: string = fn) => {
+		if (process.env.PROD) return;
+
+		writeFile(filename, data, function (err) {
+			if (err) return console.error(err);
+		});
+
+		console.debug(`Writing to ${filename}`);
+	};
+
+	return { write };
+};
