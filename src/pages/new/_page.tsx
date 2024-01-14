0bb79bbdf3a2cd3244b0ae186bb86e7fe8bc245e
timredd <timredd13@gmail.com>
2024-01-14 09:24:19 -0500

    Formatting tweaks new queue page

--- src/pages/new/_page.tsx
+++ src/pages/new/_page.tsx
@@ -1,18 +1,11 @@
 import { Server } from "@/hooks/useServer";
-
-import type { ContentItem } from "@/db/schema";
-import { useState } from "react";
-import { EditContentItem } from "./_components/EditContentItem";
+import { EmptyQueue } from "./_components/EmptyQueue";
 
 export const NewContentItemPage = () => {
-	const [selected, setSelected] = useState<ContentItem | null>(null);
-
 	return (
 		<Server>
 			<main className="container flex flex-grow py-4">
-				<div className="flex flex-auto flex-col justify-between rounded-lg border-gray-800 border-opacity-50 bg-gradient-to-b from-twilight-800 to-twilight-900 p-4">
-					<EditContentItem />
-				</div>
+				<EmptyQueue />
 			</main>
 		</Server>
 	);
