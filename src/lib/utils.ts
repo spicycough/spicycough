17166a6dcc097fe8c99f8afd733059823e60be2e
timredd <timredd13@gmail.com>
2024-01-20 11:45:14 -0500

    Add two helper functions to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,17 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const slugify = (str: string) => str.toLowerCase().replace(/\s+/g, "-");
+
+export function takeFirst<T>(array: Array<T | null>): T | null {
+	return array.find((element) => !!element || typeof element === "undefined") ?? null;
+}
+
+export function eagerFind<T, U>(array: T[], func: (item: T) => U | null): U | null {
+	for (const item of array) {
+		const result = func(item);
+		if (result !== null) {
+			return result;
+		}
+	}
+	return null;
+}
