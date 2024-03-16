daae98dc6d3066920a44b3b870d1cb64e81feeae
timredd <timredd13@gmail.com>
2024-03-16 10:34:43 -0400

    Cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,17 @@ import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import db from "@astrojs/db"
 
+// https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || "https://spicycough.com",
   output: "server",
+  compressHTML: true,
+  prefetch: true,
   integrations: [db(), tailwind({ applyBaseStyles: false })],
   adapter: cloudflare({
     mode: "directory",
     imageService: "passthrough",
     runtime: { mode: "local", type: "pages" },
   }),
-  compressHTML: true,
-  prefetch: true,
 })
+
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
-
-import { z } from "astro:content"
-import { env } from "./variables"
-
-interface ImportMeta {
-  readonly env: z.infer<typeof env>
-}
