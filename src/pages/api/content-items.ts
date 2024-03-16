c42d6080e4a8f2ff23a21f89a5825e0786cd4878
timredd <timredd13@gmail.com>
2024-03-16 10:34:07 -0400

    Get metadata and summaries working

--- src/pages/api/content-items.ts
+++ src/pages/api/content-items.ts
@@ -1,7 +1,8 @@
 import type { APIRoute } from "astro"
-import * as semantics from "@/lib/web/semantics"
+// import * as semantics from "@/lib/web/semantics"
 import * as utils from "@/lib/web/html"
-import * as fs from "fs"
+import { createSummary } from "@summaries"
+import { ContentItem, ContentItemSummary, db } from "astro:db"
 
 import { z } from "astro:content"
 import { nanoid } from "nanoid"
@@ -19,16 +20,78 @@ export const POST: APIRoute = async ({ request }) => {
   }
 
   const { url } = parsedData.data
-  const metadata = await utils.getMeta({ url })
-  console.log(metadata)
 
-  // fs.writeFileSync(`./testdata/${nanoid()}.html`, pageContent.join("\n"))
+  const resp = await fetch(url)
+  const body = await resp.text()
+
+  const metadata = await utils.getMetadata({ url })
+  const summary = await createSummary({ html: body })
+
+  const { id, ...contentItem } = await db
+    .insert(ContentItem)
+    .values({
+      id: nanoid(),
+      kind: "article",
+      author: metadata.author,
+      publishedDate: new Date(metadata.date),
+      description: metadata.description,
+      image: metadata.image,
+      lang: metadata.lang,
+      logo: metadata.logo,
+      publisher: metadata.publisher,
+      title: metadata.title,
+      url: metadata.url,
+      slug: metadata.title.toLowerCase().replace(/\s/g, "-"),
+    })
+    .returning()
+    .get()
+
+  console.log({ id, ...contentItem })
+
+  const { contentItemId: _, ...contentItemSummary } = await db
+    .insert(ContentItemSummary)
+    .values({
+      contentItemId: id,
+      type: "article",
+      summary,
+    })
+    .returning()
+    .get()
+
+  console.log({ contentItemId: id, ...contentItemSummary })
 
   return new Response(
     JSON.stringify({
-      url,
-      content: metadata,
+      ...{ contentItemId: id },
+      ...contentItem,
+      ...contentItemSummary,
     }),
     { status: 200 }
   )
 }
+
+// const queries = Object.fromEntries(
+//   metadata
+//     .map((meta) =>
+//       match(meta)
+//         // opengraph
+//         .with({ name: "og:description" }, ({ value }) => ["ogDescription", value])
+//         .with({ name: "og:image" }, ({ value }) => ["ogImage", value])
+//         .with({ name: "og:site_name" }, ({ value }) => ["ogSiteName", value])
+//         .with({ name: "og:title" }, ({ value }) => ["ogTitle", value])
+//         .with({ name: "og:type" }, ({ value }) => ["ogType", value])
+//         .with({ name: "og:updated_time" }, ({ value }) => ["ogUpdatedTime", new Date(value)])
+//         .with({ name: "og:url" }, ({ value }) => ["ogUrl", value])
+//         // twt
+//         .with({ name: "twitter:card" }, ({ value }) => ["twtCard", value])
+//         .with({ name: "twitter:creator" }, ({ value }) => ["twtCreator", value])
+//         .with({ name: "twitter:site" }, ({ value }) => ["twtSite", value])
+//         // facebook
+//         .with({ name: "fb:app_id" }, ({ value }) => ["fbAppId", value])
+//         .with({ name: "fb:pages" }, ({ value }) => ["fbPages", value])
+//         .otherwise(() => undefined)
+//     )
+//     .filter(Boolean)
+// )
+
+// await db.insert(ContentItem).values(
