2a78bb348a68be4fa4fb000e9696e5fa0ffcb24b
timredd <timredd13@gmail.com>
2024-03-27 01:01:54 -0400

    Add types to top-level .d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "file-extension" {
+  const fileExtension: (value: string) => string
+  export default fileExtension
+}
+
+declare module "top-user-agents"
