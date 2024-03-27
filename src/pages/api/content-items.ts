42046306e1725685cbe3b2b22649021c1f721eea
timredd <timredd13@gmail.com>
2024-03-26 21:09:19 -0400

    Fix type errors

--- src/pages/api/content-items.ts
+++ src/pages/api/content-items.ts
@@ -51,16 +51,6 @@ export const POST: APIRoute = async ({ request }) => {
     )
   }
 
-  await db.insert(ContentItem).values({
-    id: nanoid(),
-    kind: "article",
-    url: "https://www.nature.com/articles/s41591-022-02001-z",
-    title: "Long-term neurologic outcomes of COVID-19",
-    author: "Ziyad Al-Aly",
-    slug: "long-term-neurologic-outcomes-of-covid-19",
-    publishedDate: new Date("2022-03-01"),
-  })
-
   const { id, ...contentItem } = await db
     .insert(ContentItem)
     .values({
