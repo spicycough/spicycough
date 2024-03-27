cb41de5fa28f5111d7cfd732cf86cf54570aef72
timredd <timredd13@gmail.com>
2024-03-27 01:01:33 -0400

    Tweak seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,10 +1,12 @@
 import { db, ContentItem } from "astro:db"
+import { nanoid } from "nanoid"
 
 // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(ContentItem).values({
+    id: nanoid(),
     kind: "article",
-    permalink: "https://www.nature.com/articles/s41591-022-02001-z",
+    url: "https://www.nature.com/articles/s41591-022-02001-z",
     title: "Long-term neurologic outcomes of COVID-19",
     author: "Ziyad Al-Aly",
     slug: "long-term-neurologic-outcomes-of-covid-19",
